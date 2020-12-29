2b254c10219609f3a931994ec97d9920e81da3b7
awpala <adam.w.pala@gmail.com>
2020-12-28 21:47:32 -0600

    Add helper function getLetterMatchCount and red tests

---
+++ src/helpers/index.js
@@ -0,0 +1,9 @@
+/**
+ * @method getLetterMatchCount
+ * @param {string} guessedWord - Guessed word.
+ * @param {string} secretWord - Secret word.
+ * @returns {number} - Number of letters matched between guessed word and secret word.
+ */
+export const getLetterMatchCount = (guessedWord, secretWord) => {
+
+}
