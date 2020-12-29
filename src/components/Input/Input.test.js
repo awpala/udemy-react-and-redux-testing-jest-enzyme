caa1976df8af2132fa8468453388660a5f09a5c5
awpala <adam.w.pala@gmail.com>
2020-12-29 17:55:11 -0600

    Add red tests for Input component, 'word not guessed' condition

--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -25,28 +25,33 @@ const setup = (initialState={}) => {
 
 describe('render', () => {
     describe('word has not been guessed', () => {
+        let wrapper;
+        beforeEach(() => {
+            const initialState = { isSuccess: false };
+            wrapper = setup(initialState);
+        });
         test('renders component without error', () => {
-
+            const component = findByTestAttr(wrapper, 'component-input');
+            expect(component.length).toBe(1);
         });
         test('renders input box', () => {
-
+            const inputBox = findByTestAttr(wrapper, 'input-box');
+            expect(inputBox.length).toBe(1);
         });
         test('renders submit button', () => {
-
+            const submitButton = findByTestAttr(wrapper, 'submit-button');
+            expect(submitButton.length).toBe(1);
         });
     });
     describe('word has been guessed', () => {
         test('renders component without error', () => {
 
         });
-        test('renders input box', () => {
+        test('does not render input box', () => {
 
         });
-        test('renders submit button', () => {
+        test('does not render submit button', () => {
 
         });
     });
-});
-describe('update state', () => {
-
 });
