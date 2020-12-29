2d7039881814896159b20cb907cd5dc287c8150c
awpala <adam.w.pala@gmail.com>
2020-12-28 21:49:16 -0600

    Implement helper function getLetterMatchCount

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -5,5 +5,7 @@
  * @returns {number} - Number of letters matched between guessed word and secret word.
  */
 export const getLetterMatchCount = (guessedWord, secretWord) => {
-
+    const secretLetterSet = new Set(secretWord.split(''));
+    const guessedLetterSet = new Set(guessedWord.split(''));
+    return [...secretLetterSet].filter(letter => guessedLetterSet.has(letter)).length;
 }
