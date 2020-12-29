513456613100dd7bce0de229e1eb2de6ce72c789
awpala <adam.w.pala@gmail.com>
2020-12-29 17:22:09 -0600

    Add empty tests for Input component

--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -2,4 +2,45 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import { findByTestAttr } from '../../../test/testUtils';
+import Input from './Input';
 
+// N.B. Input is a connected component, which requires a more complex setup than components GuessedWords and Congrats
+
+/**
+ * Factory function to create a ShallowWrapper for the Input component.
+ * @function setup
+ * @param {object} initialState - Initial state for this setup.
+ * @returns {ShallowWrapper} 
+ */
+const setup = (initialState={}) => {
+    const wrapper = shallow(<Input />);
+
+};
+
+describe('render', () => {
+    describe('word has not been guessed', () => {
+        test('renders component without error', () => {
+
+        });
+        test('renders input box', () => {
+
+        });
+        test('renders submit button', () => {
+
+        });
+    });
+    describe('word has been guessed', () => {
+        test('renders component without error', () => {
+
+        });
+        test('renders input box', () => {
+
+        });
+        test('renders submit button', () => {
+
+        });
+    });
+});
+describe('update state', () => {
+
+});
