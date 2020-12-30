91ad4a6ebf26c783a1cb8fd98e693a4b011b361b
awpala <adam.w.pala@gmail.com>
2020-12-30 10:19:09 -0600

    Add redux-thunk middleware

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,7 +1,8 @@
 import checkPropTypes from 'check-prop-types';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import rootReducer from '../src/reducers/';
+import { middlewares } from '../src/configureStore';
 
 /**
  * Create a testing store with imported reducers, middleware, and initial state.
@@ -10,7 +11,10 @@ import rootReducer from '../src/reducers/';
  * @param {object} initialState - Initial state for store.
  * @returns {Store} - Redux store.
  */
-export const storeFactory = (initialState) => createStore(rootReducer, initialState);
+export const storeFactory = (initialState) => {
+    const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+    return createStoreWithMiddleware(rootReducer, initialState);
+}
 
 /**
  * Return node(s) with the given data-test attribute.
