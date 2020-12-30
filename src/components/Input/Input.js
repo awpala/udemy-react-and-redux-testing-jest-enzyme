25fab8acf8302b07c757532db64f1f8632f26d54
awpala <adam.w.pala@gmail.com>
2020-12-29 18:04:29 -0600

    Implement Input component, 'word not guessed' condition

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-class Input extends Component {
+class Input extends Component {  
     render() {
+        const contents = this.props.isSuccess
+            ? null
+            : (
+                <form className="form-inline">
+                    <input
+                        data-test="input-box"
+                        className="mb-2 mx-sm-3"
+                        type="text"
+                        placeholder="enter guess"
+                    />
+                    <button
+                        data-test="submit-button"
+                        className="btn btn-primary mb-2"
+                        type="submit"
+                    >
+                        Submit
+                    </button>
+                </form>
+            );
+        
         return (
-            <div />
+            <div data-test="component-input">
+                {contents}
+            </div>
         );
     }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = ({ isSuccess }) => {
     return {
-
+        isSuccess,
     };
 };
 
